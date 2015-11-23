fc1bba4dcd6ba3e15cd59b2f7b5dfa13901ef3e1
mathproblemsolved <customerservice@mathproblemsolved.com>
2015-11-22 23:02:35 -0500

    fixing the randomness of answers

--- scripts/quiz_deluxe.pl
+++ scripts/quiz_deluxe.pl
@@ -5,6 +5,8 @@
 use warnings;
 use strict;
 use Switch;
+use File::Find;
+use File::Copy;
 
 my $numArgs = $#ARGV + 1;
 
@@ -41,12 +43,15 @@ my $question_directory = $attribute_hash_ref{"question_directory"};
 my $answer_directory = $attribute_hash_ref{"answer_directory"};
 my $hints_directory = $attribute_hash_ref{"hints_directory"};
 my $options_directory = $attribute_hash_ref{"options_directory"};
+my $options_directory_forQuiz;
 my $logical_hints_directory = $attribute_hash_ref{"logical_hints_directory"};
 my $problem_string = $attribute_hash_ref{"problem_string"};
 my $real_question_directory;
 my $real_answer_directory;
 my $real_hints_directory;
 my $real_options_directory;
+my $real_options_directory_forQuiz;
+
 
 #print " sql_id = $sql_id ,  $attribute_hash_ref->{"sql_id"};\n";
 
@@ -97,11 +102,13 @@ my $product_index = $start_DB_id + $i - 1 ;
   $real_answer_directory = ${root_directory}."problem${i}"."/answer" ;
   $real_hints_directory = ${root_directory}."problem${i}"."/hints";
   $real_options_directory =${root_directory}."problem${i}"."/options";
+  $real_options_directory_forQuiz = ${root_directory}."problem${i}"."/options/forQuiz";
   $question_directory = ${logical_root_directory}."problem${i}"."/question" ;
   $answer_directory = ${logical_root_directory}."problem${i}"."/answer" ;
   $hints_directory = ${logical_root_directory}."problem${i}"."/hints";
   $options_directory ="https://mathproblemsolved.com/".${logical_root_directory}."problem${i}"."/options";
-
+  $options_directory_forQuiz ="https://mathproblemsolved.com/".${logical_root_directory}."problem${i}"."/options/forQuiz";
+ 
 opendir  $QUESTION_DIR, $real_question_directory or next PROBLEM;
 opendir  $OPTIONS_DIR, $real_options_directory or next PROBLEM;
 
@@ -124,9 +131,14 @@ if ( $solution_directory eq "") {
         $path_to_solution = "<a href=".q?"https://www.mathproblemsolved.com/index.php/?.$solution_directory."/".$product_index."-".$product.q?"  target="_blank"/>Click for Complete solution</a>? ;
 
 }
-# some questions have been rewritten to deal with multiple choice questions. 
+
+
+# some questions have been rewritten to deal with multiple choice questions (esp in trig - where the question contains the identity to prove, while
+# multiple choice questions require the Right Hand Side to be in the options.
 #It is called new_question1.jpg (or whatever)
 #If it exists use that - else use the original question
+
+# Check if our Quiz option directory exists if not create it.
 if (-e $new_base_path) {
 
     #file exists - set variable and other things
@@ -139,6 +151,25 @@ if (-e $new_base_path) {
 next PROBLEM;
 };
 
+#Check if option files exist - otherwise not suited for multiple choice quiz
+my $option_string_check = $real_options_directory.q?/option?.${i}.q?_1.jpg? ;
+  if (! -e $option_string_check) {
+# Options do not exist - move on to next problem
+next PROBLEM;
+};
+
+# Check if our Quiz option directory exists if not create it.
+if (-e $real_options_directory_forQuiz ) {
+#print "Directory  $real_options_directory_forQuiz  already exists \n";
+
+} else {
+#Create
+mkdir(  $real_options_directory_forQuiz  ) or die "Couldn't create  $real_options_directory_forQuiz  directory, $!";
+#print "Directory  $real_options_directory_forQuiz  created successfully\n";
+};
+
+
+
 my $first;
 my $second;
 my $third;
@@ -147,32 +178,33 @@ my $first_correct;
 my $second_correct;
 my $third_correct;
 my $fourth_correct;
+my $random0thru3 = int(rand(4));
+#print "RANDOM number is $random0thru3 \n" ;
 
-#my $random0thru3 = int(rand(4));
-#No radomness -> always go with case 0
-my $random0thru3  = 0;
 
-#print " random number is $random0thru3 \n";
 switch($random0thru3){
    case 0    { $first = 1; $second = 2; $third = 3; $fourth = 4;  $first_correct = "TRUE"; $second_correct = "FALSE"; $third_correct = "FALSE" ; $fourth_correct="FALSE";}
-   case 1    { $first = 2; $second = 1; $third = 3; $fourth = 4;  $first_correct = "FALSE"; $second_correct = "TRUE"; $third_correct = "FALSE" ; $fourth_correct="FALSE";}
-   case 2    { $first = 3; $second = 2; $third = 1; $fourth = 4;  $first_correct = "FALSE"; $second_correct = "FALSE"; $third_correct = "TRUE" ; $fourth_correct="FALSE";}
-   case 3    { $first = 4; $second = 1; $third = 2; $fourth = 1;  $first_correct = "FALSE"; $second_correct = "FALSE"; $third_correct = "FALSE" ; $fourth_correct="TRUE";}
+   case 1    { $first = 4; $second = 1; $third = 2; $fourth = 3;  $first_correct = "FALSE"; $second_correct = "TRUE"; $third_correct = "FALSE" ; $fourth_correct="FALSE";}
+   case 2    { $first = 3; $second = 4; $third = 1; $fourth = 2;  $first_correct = "FALSE"; $second_correct = "FALSE"; $third_correct = "TRUE" ; $fourth_correct="FALSE";}
+   case 3    { $first = 2; $second = 3; $third = 4; $fourth = 1;  $first_correct = "FALSE"; $second_correct = "FALSE"; $third_correct = "FALSE" ; $fourth_correct="TRUE";}
    else      { print "Bad random number which is not 0 or 1 or 2 or 3" }
 } ;
 
-my $option_string_check = $real_options_directory.q?/option?.${i}.q?_1.jpg? ;
-  if (! -e $option_string_check) {
-# Options do not exist - move on to next problem
-next PROBLEM;
-};
+# Copy files to Options directory
+
+copy("$real_options_directory/option${i}_1.jpg","$real_options_directory_forQuiz/option${i}_$first.jpg");
+copy("$real_options_directory/option${i}_2.jpg","$real_options_directory_forQuiz/option${i}_$second.jpg");
+copy("$real_options_directory/option${i}_3.jpg","$real_options_directory_forQuiz/option${i}_$third.jpg");
+copy("$real_options_directory/option${i}_4.jpg","$real_options_directory_forQuiz/option${i}_$fourth.jpg");
 
 
+
+#Now set the solutions
   
-  $option1_string =  q?"","","?.$first_correct.q?",<p><img src="?.$options_directory.q?/option?.${i}.q?_1.jpg" alt="" /></p>,"0","","","","Correct. Check the solution if needed",""?;
-  $option2_string =  q?"","","?.$second_correct.q?",<p><img src="?.$options_directory.q?/option?.${i}.q?_2.jpg" alt="" /></p>,"0","","","","Incorrect: Check solution",""?;
-  $option3_string =  q?"","","?.$third_correct .q?",<p><img src="?.$options_directory.q?/option?.${i}.q?_3.jpg" alt="" /></p>,"0","","","","Incorrect: Check solution",""?;
-  $option4_string =  q?"","","?.$fourth_correct.q?",<p><img src="?.$options_directory.q?/option?.${i}.q?_4.jpg" alt="" /></p>,"0","","","","Incorrect: Check solution",""?;
+  $option1_string =  q?"","","?.$first_correct.q?",<p><img src="?.$options_directory_forQuiz.q?/option?.${i}.q?_1.jpg" alt="" /></p>,"0","","","","Correct. Check the solution if needed",""?;
+  $option2_string =  q?"","","?.$second_correct.q?",<p><img src="?.$options_directory_forQuiz.q?/option?.${i}.q?_2.jpg" alt="" /></p>,"0","","","","Incorrect: Check solution",""?;
+  $option3_string =  q?"","","?.$third_correct .q?",<p><img src="?.$options_directory_forQuiz.q?/option?.${i}.q?_3.jpg" alt="" /></p>,"0","","","","Incorrect: Check solution",""?;
+  $option4_string =  q?"","","?.$fourth_correct.q?",<p><img src="?.$options_directory_forQuiz.q?/option?.${i}.q?_4.jpg" alt="" /></p>,"0","","","","Incorrect: Check solution",""?;
 
 
   print "$problem_string\r\n"."$option1_string\r\n"."$option2_string\r\n"."$option3_string\r\n"."$option4_string\r\n";
